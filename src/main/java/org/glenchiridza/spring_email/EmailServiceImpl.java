d47f8ea18f985baa8a5495c3158c136debf1fd9b
glenchiridza <glenchiridz@gmail.com>
2024-04-04 23:20:46 +0200

    email service impl

---
+++ src/main/java/org/glenchiridza/spring_email/EmailServiceImpl.java
@@ -0,0 +1,60 @@
+package org.glenchiridza.spring_email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    @Override
+    public String sendSimpleMail(EmailDetails details) {
+        try{
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(details.getRecipient());
+            mailMessage.setText(details.getMessageBody());
+            mailMessage.setSubject(details.getSubject());
+
+            javaMailSender.send(mailMessage);
+            return "Mail send successful";
+        }catch (Exception e){
+            return "Error on sending email";
+        }
+    }
+
+    @Override
+    public String sendMailWithAttachment(EmailDetails details) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+        try {
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(details.getRecipient());
+            mimeMessageHelper.setText(details.getMessageBody());
+            mimeMessageHelper.setSubject(details.getSubject());
+
+            FileSystemResource file = new FileSystemResource(new File(details.getAttachment()));
+            mimeMessageHelper.addAttachment(file.getFilename(),file);
+
+            javaMailSender.send(mimeMessage);
+            return "Mail send successfully";
+        }catch (MessagingException e){
+            return "Error while sending email";
+        }
+    }
+}
